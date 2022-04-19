0848fc20d0fd40bde56d7794d050521a7ced8f2a
Markteisha Gill <markteishagill@Markteishas-MacBook-Pro.local>
2022-04-19 14:27:04 -0400

    final commit

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const search = document.querySelector('#search');
 const address = document.querySelector('#address');
 const Apikey = document.querySelector('#api-key');
 const norad = document.querySelector('#norad');
-const timewhen = document.querySelector('.when');
+const ourContent = document.querySelector(".content");
+const satelliteDataSection = document.createElement("div");
 
 search.addEventListener('click', function(){
     // console.log('clicked');
@@ -24,9 +25,9 @@ search.addEventListener('click', function(){
         const long = json.features[0].center[0];
         const lati = json.features[0].center[1];
         // console.log(json)
-        console.log(data.features[0]); 
-            let dataLat = data.features[0].center[1];
-            let dataLong = data.features[0].center[0];// to get latitude and longitude
+        console.log(json.features[0]); 
+            let dataLat = json.features[0].center[1];
+            let dataLong = json.features[0].center[0];// to get latitude and longitude
         const satelliteUrl = `https://satellites.fly.dev/passes/${norad.value}?lat=${lati}&lon=${long}&limit=1&days=15&visible_only=true`;
         
         const coded = encodeURI(satelliteUrl);
@@ -36,27 +37,31 @@ search.addEventListener('click', function(){
         .then((res) => res.json())
 
         .then((json) => {
-            console.log('satelliteJson', json);
-            const rise = json[0].rise.utc_datetime;
-            const culm = json[0].culmination.utc_datetime;
-            const set = json[0].set.utc_datetime;
-            console.log('rise:',rise);
-            console.log('cul:',culm);
-            console.log('set:', set)
+            if (json.length === 0) {
+                window.alert(
+                  "This satellite won't be passing this location anytime soon. Try a different satellite/location combination!"
+                );
+              }
+              if (json.length !== 0) {
+                satelliteDataSection.innerHTML =
+                  '<div class="section"><div class="row"><h2 id="our-satellite-data-words">Our Satellite Data</h2></div><div class="row"><span>It Rises---></span><span id="rise"></span></div><div class="row"><span>It Culminates---></span><span id="culminate"></span></div><div class="row"><span>It Sets---></span><span id="set"></span></div></div>';
+                ourContent.appendChild(satelliteDataSection);
+  
+                //query select the rise div
+                const riseSpan = document.querySelector("#rise");
+                //set the innertext of the rise div to the data from api
+                riseSpan.innerText = json[0].rise.utc_datetime;
+                //query select the set div
+                const setSpan = document.querySelector("#set");
+                //set the innertext of the set div to the data from api
+                setSpan.innerText = json[0].set.utc_datetime;
+                // query select the culminate div
+                const culminateSpan = document.querySelector("#culminate");
+                //set the innertext of the culminate div to the data from api
+                culminateSpan.innerText = json[0].culmination.utc_datetime;
+              }
+        });
 
-    let newHtml = 
-        `<div class = "satellite-address">
-        <h2>Info/Reading</h2>
-            <h3>Rise<h3>
-            <p>${rise}</p>
-            <h3>Culminate<h3>
-            <p>${culm}</p>
-            <h3>Set<h3>
-            <p>${set}</p>
-        <div>`  
-    whenInfo.innerHTML +=newHtml;
-        })
-
-    })
+    });
 
 })
